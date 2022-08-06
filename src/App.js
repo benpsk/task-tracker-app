79fd13faf5e5c1adcb539f450e40ce84456c2ed1
Ben Gunn <bengunn.dev@gmail.com>
2022-08-06 15:31:02 +0630

    for online-demo & update title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 import About from './components/About'
 
+const API = 'https://my-json-server.typicode.com/benpsk/task-tracker-server';
+
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
@@ -21,7 +23,7 @@ const App = () => {
 
   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
+    const res = await fetch(`${API}/tasks`)
     const data = await res.json()
 
     return data
@@ -29,7 +31,7 @@ const App = () => {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const res = await fetch(`${API}/tasks/${id}`)
     const data = await res.json()
 
     return data
@@ -37,7 +39,7 @@ const App = () => {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch('http://localhost:5000/tasks', {
+    const res = await fetch(`${API}/tasks`, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
@@ -56,7 +58,7 @@ const App = () => {
 
   // Delete Task
   const deleteTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`${API}/tasks/${id}`, {
       method: 'DELETE',
     })
     //We should control the response status to decide if we will change the state or not.
@@ -70,7 +72,7 @@ const App = () => {
     const taskToToggle = await fetchTask(id)
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`${API}/tasks/${id}`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json',
